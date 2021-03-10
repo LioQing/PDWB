8156ed01e1052b00f05a76e3e2cb2b4bd7ee55e0
LioQing <46854695+LioQing@users.noreply.github.com>
2021-03-10 19:56:47 +0800

    Add bad_apple_ascii macos

--- bad_apple_ascii/main.cpp
+++ bad_apple_ascii/main.cpp
@@ -3,33 +3,41 @@
 #include <string>
 #include <chrono>
 
-#ifdef _WIN32
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
 	#include <windows.h>
 #else
 	#include <unistd.h>
+    #if __APPLE__
+        #include <thread>
+    #endif
 #endif
 
 // clear
-#ifdef _WIN32
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
     #define ClearScreen() SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0, 0 })
 #else
 	#define ClearScreen() printf("\033[%d;%dH", 1, 1)
 #endif
 
 // sleep
-#ifdef _WIN32
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
 	#define sleep(ms) Sleep(ms)
 #else
 	#define sleep(ms) usleep(ms * 1000)
 #endif
 
 // play music
-#ifdef _WIN32
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
     #define play_sound(filename) PlaySound(TEXT(filename), NULL, SND_FILENAME | SND_ASYNC)
 #endif
 
 // time step
 #define TIMESTEP 1000.0 / 15.0
+
+void afplay()
+{
+    system("afplay bad_apple.wav");
+}
 
 int main()
 {
@@ -40,9 +48,12 @@ int main()
     uint32_t et = 0;
     uint32_t td = 0;
 
-    #ifdef _WIN32
+    #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
     play_sound("bad_apple.wav");
     sleep(120);
+    #elif __APPLE__
+    std::thread t(afplay);
+    sleep(200);
     #endif
     begin = std::chrono::steady_clock::now();
     for (uint32_t n = 1;; ++n)
@@ -61,6 +72,7 @@ int main()
     }
 
     video_file.close();
+    t.join();
 
     std::cin.get();
 
