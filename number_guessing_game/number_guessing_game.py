752d664c543756f8dc2546204bcca082367986b6
Lio <46854695+LioQing@users.noreply.github.com>
2021-10-25 20:03:10 +0800

    number_guessing_game.py I now know more Python

--- number_guessing_game/number_guessing_game.py
+++ number_guessing_game/number_guessing_game.py
@@ -1,5 +1,6 @@
 import random
 
+random.seed()
 lower = 1
 upper = 100
 n = random.randint(lower, upper)
@@ -9,19 +10,17 @@ while input_num != n:
     while True:
         try:
             input_num = int(input("Make a guess (%d - %d): " % (lower, upper)))
+            if input_num < lower or input_num > upper:
+                raise ValueError
+            break
         except ValueError:
             continue
-        
-        if input_num < lower or input_num > upper:
-            continue
-        else:
-            break
     
     if input_num > n:
-        print("%d is too large" % input_num)
+        print(f"{input_num} is too large")
         upper = input_num - 1
     elif input_num < n:
-        print("%d is too small" % input_num)
+        print(f"{input_num} is too small")
         lower = input_num + 1
     else:
         print("You are correct")
