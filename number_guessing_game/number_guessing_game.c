7db2d6b4ef65621868fea26a2cc6ae9849709676
GruelingPine185 <66073333+GruelingPine185@users.noreply.github.com>
2022-06-30 09:42:18 -0400

    added structure to replace underused ints

--- number_guessing_game/number_guessing_game.c
+++ number_guessing_game/number_guessing_game.c
@@ -2,23 +2,30 @@
 #include <stdlib.h>
 #include <time.h>
 
+typedef struct bound bound;
+
+struct bound {
+    short lower;
+    short upper;
+};
+
+
 int main(int argc, char** argv) {
-    int lower = 1, upper = 100;
-    int n;
+    srand(time(NULL));
+    bound range = {1, 100};
+    int n = rand() % range.upper + 1;
     int input = 0;
 
-    srand(time(NULL));
-    n = rand() % 100 + 1;
     do {
         fflush(stdin);
-        printf("Make a guess (%d - %d): ", lower, upper);
+        printf("Make a guess (%d - %d): ", range.upper, range.lower);
         if (!scanf("%d", &input)) continue;
         if (input > n) {
             printf("%d is too large\n", input);
-            upper = input - 1;
+            range.upper = input - 1;
         } else if (input < n) {
             printf("%d is too small\n", input);
-            lower = input + 1;
+            range.lower = input + 1;
         }
     } while (input != n);
 
